51b1c472b3e2a2226082a42a4d92627529c3ba94
mrverma73575a <mrverma73575a@gmail.com>
2024-12-24 14:41:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,8 +25,8 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+Ffyql2XZsndlNz
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-4624239556').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mohitverma:mohitverma@mohitverma.c5xz5.mongodb.net/?retryWrites=true&w=majority&appName=mohitverma")
-DATABASE_NAME = environ.get('DATABASE_NAME', "mohitverma")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://verma12:verma12@verma1.eyrz9.mongodb.net/?retryWrites=true&w=majority&appName=verma1")
+DATABASE_NAME = environ.get('DATABASE_NAME', "verma1")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
